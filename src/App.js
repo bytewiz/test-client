fe2d33d14e84314ad7569cc86da1be0e2b891e77
Niels-tame <niels@tamethat.com>
2016-11-10 14:52:18 +0100

    fixed a bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getToken } from "./service/api"
+import { getToken, getAccount } from "./service/api"
 
 
 export default class App extends Component {
@@ -24,13 +24,16 @@ export default class App extends Component {
       console.log("res", response.body)
       this.setState({response: "you created a token! :-)"})
     })
+
+
+
   }
   render() {
     return (
       <div>
         <h1>Hello, world.</h1>
-        {this.state.response}
-        <form onSubmit={this._submit}>
+
+        <form onSubmit={this._submit.bind(this)}>
           <label>
             username
             <input type="text" onChange={(e) => this.setState({email: e.target.value})}/>
@@ -40,7 +43,9 @@ export default class App extends Component {
             <input type="password" onChange={(e) => this.setState({password: e.target.value})}/>
           </label>
           <button type="submit">login</button>
+
         </form>
+        {this.state.response}
       </div>
 
     );
